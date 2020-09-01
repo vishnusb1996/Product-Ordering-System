8ceb5cbe69227f95358e41048d60c9cde2670b95
vishnusb1996 <52482641+vishnusb1996@users.noreply.github.com>
2020-09-01 18:02:06 +0530

    first commit

---
+++ dbconstants.py
@@ -0,0 +1,2 @@
+SELECT_ALL_CATEGORIES_SQL = 'SELECT categoryid, categoryname, description FROM categories';
+# SELECT_ALL_PRODUCTS_SQL = 'SELECT productid, productname,CategoryID,UnitPrice FROM product',
