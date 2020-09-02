2fd6b5f3dcc28483f4b44e3848ad05162492834a
vishnusb1996 <52482641+vishnusb1996@users.noreply.github.com>
2020-09-02 10:48:19 +0530

    Updated the file category

--- main.py
+++ main.py
@@ -5,35 +5,35 @@ from flask import jsonify
 from flask import flash, request
 import dbconstants
 
-@app.route('/createcategory', methods=['POST'])
-def add_category():
-	try:
-		_json = request.json
-		_category_name = _json['categoryname']
-		_cat_description = _json['description']
-		# validate the received values
-		if _category_name and _cat_description and request.method == 'POST':
+# @app.route('/createcategory', methods=['POST'])
+# def add_category():
+# 	try:
+# 		_json = request.json
+# 		_category_name = _json['categoryname']
+# 		_cat_description = _json['description']
+# 		# validate the received values
+# 		if _category_name and _cat_description and request.method == 'POST':
 		
-			# save edits
-			sql = "INSERT INTO categories(categoryname, description) VALUES(%s, %s)"
-			data = (_category_name, _cat_description)
-			conn = mysql.connect()
-			cursor = conn.cursor()
-			cursor.execute(sql, data)
-			conn.commit()
-			resp = jsonify('category added successfully!')
-			resp.status_code = 200
-			return resp
-		else:
-			return not_found()
-	except Exception as e:
-		print(e)
-	finally:
-		cursor.close() 
-		conn.close()
+# 			# save edits
+# 			sql = "INSERT INTO categories(categoryname, description) VALUES(%s, %s)"
+# 			data = (_category_name, _cat_description)
+# 			conn = mysql.connect()
+# 			cursor = conn.cursor()
+# 			cursor.execute(sql, data)
+# 			conn.commit()
+# 			resp = jsonify('category added successfully!')
+# 			resp.status_code = 200
+# 			return resp
+# 		else:
+# 			return not_found()
+# 	except Exception as e:
+# 		print(e)
+# 	finally:
+# 		cursor.close() 
+# 		conn.close()
 
-@app.route('/categories')
-def categories():
+@app.route('/products')
+def products():
 	try:
 		conn = mysql.connect()
 		cursor = conn.cursor(pymysql.cursors.DictCursor)
@@ -48,132 +48,132 @@ def categories():
 		cursor.close() 
 		conn.close()
 
-@app.route('/category/<int:id>')
-def category(id):
-	try:
-		conn = mysql.connect()
-		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		cursor.execute("SELECT categoryid, categoryname, description FROM categories WHERE categoryid=%s", id)
-		row = cursor.fetchone()
-		resp = jsonify(row)
-		resp.status_code = 200
-		return resp
-	except Exception as e:
-		print(e)
-	finally:
-		cursor.close() 
-		conn.close()
+# @app.route('/categories')
+# def categories():
+# 	try:
+# 		conn = mysql.connect()
+# 		cursor = conn.cursor(pymysql.cursors.DictCursor)
+# 		cursor.execute(dbconstants.SELECT_ALL_CATEGORIES_SQL)
+# 		rows = cursor.fetchall()
+# 		resp = jsonify(rows)
+# 		resp.status_code = 200
+# 		return resp
+# 	except Exception as e:
+# 		print(e)
+# 	finally:
+# 		cursor.close() 
+# 		conn.close()
 
-@app.route('/UpdateCategory', methods=['POST'])
-def update_category():
-	try:
-		_json = request.json
-		_category_id = _json['categoryid']
-		_category_name = _json['categoryname']
-		_category_description = _json['description']
+# @app.route('/category/<int:id>')
+# def category(id):
+# 	try:
+# 		conn = mysql.connect()
+# 		cursor = conn.cursor(pymysql.cursors.DictCursor)
+# 		cursor.execute("SELECT categoryid, categoryname, description FROM categories WHERE categoryid=%s", id)
+# 		row = cursor.fetchone()
+# 		resp = jsonify(row)
+# 		resp.status_code = 200
+# 		return resp
+# 	except Exception as e:
+# 		print(e)
+# 	finally:
+# 		cursor.close() 
+# 		conn.close()
+
+# @app.route('/UpdateCategory', methods=['POST'])
+# def update_category():
+# 	try:
+# 		_json = request.json
+# 		_category_id = _json['categoryid']
+# 		_category_name = _json['categoryname']
+# 		_category_description = _json['description']
 		
-		# validate the received values
-		if _category_id and _category_name and _category_description and request.method == 'POST':
+# 		# validate the received values
+# 		if _category_id and _category_name and _category_description and request.method == 'POST':
 			
-			# save edits
-			sql = "UPDATE categories SET categoryname=%s, description=%s WHERE categoryid=%s"
-			data = (_category_name, _category_description, _category_id)
-			conn = mysql.connect()
-			cursor = conn.cursor()
-			cursor.execute(sql, data)
-			conn.commit()
-			resp = jsonify('Category updated successfully!')
-			resp.status_code = 200
-			return resp
-		else:
-			return not_found()
-	except Exception as e:
-		print(e)
-	finally:
-		cursor.close() 
-		conn.close()
+# 			# save edits
+# 			sql = "UPDATE categories SET categoryname=%s, description=%s WHERE categoryid=%s"
+# 			data = (_category_name, _category_description, _category_id)
+# 			conn = mysql.connect()
+# 			cursor = conn.cursor()
+# 			cursor.execute(sql, data)
+# 			conn.commit()
+# 			resp = jsonify('Category updated successfully!')
+# 			resp.status_code = 200
+# 			return resp
+# 		else:
+# 			return not_found()
+# 	except Exception as e:
+# 		print(e)
+# 	finally:
+# 		cursor.close() 
+# 		conn.close()
 
-@app.route('/DeleteCategory/<int:id>')
-def delete_category(id):
-	try:
-		conn = mysql.connect()
-		cursor = conn.cursor()
-		cursor.execute("DELETE FROM categories WHERE categoryid=%s", (id,))
-		conn.commit()
-		resp = jsonify('CategoryDeleted deleted successfully!')
-		resp.status_code = 200
-		return resp
-	except Exception as e:
-		print(e)
-	finally:
-		cursor.close() 
-		conn.close()
+# @app.route('/DeleteCategory/<int:id>')
+# def delete_category(id):
+# 	try:
+# 		conn = mysql.connect()
+# 		cursor = conn.cursor()
+# 		cursor.execute("DELETE FROM categories WHERE categoryid=%s", (id,))
+# 		conn.commit()
+# 		resp = jsonify('CategoryDeleted deleted successfully!')
+# 		resp.status_code = 200
+# 		return resp
+# 	except Exception as e:
+# 		print(e)
+# 	finally:
+# 		cursor.close() 
+# 		conn.close()
 
-@app.route('/GetAllCategories')
-def GetAllCategories():
-	try:
-		conn = mysql.connect()
-		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		cursor.callproc("GetAllCategories")
-		rows = cursor.fetchall()
-		resp = jsonify(rows)
-		resp.status_code = 200
-		return resp
-	except Exception as e:
-		print(e)
-	finally:
-		cursor.close() 
-		conn.close()
+# @app.route('/GetAllCategories')
+# def GetAllCategories():
+# 	try:
+# 		conn = mysql.connect()
+# 		cursor = conn.cursor(pymysql.cursors.DictCursor)
+# 		cursor.callproc("GetAllCategories")
+# 		rows = cursor.fetchall()
+# 		resp = jsonify(rows)
+# 		resp.status_code = 200
+# 		return resp
+# 	except Exception as e:
+# 		print(e)
+# 	finally:
+# 		cursor.close() 
+# 		conn.close()
 
-@app.route('/GetCategoryById/<int:id>')
-def GetCategoryById(id):
-	try:
-		conn = mysql.connect()
-		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		args = (id,)
-		cursor.callproc("GetCategoryById", args)
-		rows = cursor.fetchall()
-		resp = jsonify(rows)
-		resp.status_code = 200
-		return resp
-	except Exception as e:
-		print(e)
-	finally:
-		cursor.close() 
-		conn.close()
+# @app.route('/GetCategoryById/<int:id>')
+# def GetCategoryById(id):
+# 	try:
+# 		conn = mysql.connect()
+# 		cursor = conn.cursor(pymysql.cursors.DictCursor)
+# 		args = (id,)
+# 		cursor.callproc("GetCategoryById", args)
+# 		rows = cursor.fetchall()
+# 		resp = jsonify(rows)
+# 		resp.status_code = 200
+# 		return resp
+# 	except Exception as e:
+# 		print(e)
+# 	finally:
+# 		cursor.close() 
+# 		conn.close()
 
 
-@app.route('/GetAllCustomers')
-def getAllCustomers():
-	try:
-		conn = mysql.connect()
-		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		cursor.execute('SELECT * FROM customers')
-		rows = cursor.fetchall()
-		resp = jsonify(rows)
-		resp.status_code = 200
-		return resp
-	except Exception as e:
-		print(e)
-	finally:
-		cursor.close() 
-		conn.close()
-
-@app.route('/products')
-def products():
-	try:
-		conn = mysql.connect()
-		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		cursor.execute('SELECT ProductName FROM product')
-		rows = cursor.fetchall()
-		resp = jsonify(rows)
-		resp.status_code = 200
-		return resp
-	except Exception as e:
-		print(e)
-	finally:
-		cursor.close() 
-		conn.close()
+# @app.route('/GetAllCustomers')
+# def getAllCustomers():
+# 	try:
+# 		conn = mysql.connect()
+# 		cursor = conn.cursor(pymysql.cursors.DictCursor)
+# 		cursor.execute('SELECT * FROM customers')
+# 		rows = cursor.fetchall()
+# 		resp = jsonify(rows)
+# 		resp.status_code = 200
+# 		return resp
+# 	except Exception as e:
+# 		print(e)
+# 	finally:
+# 		cursor.close() 
+# 		conn.close()
 
 
 @app.errorhandler(404)
