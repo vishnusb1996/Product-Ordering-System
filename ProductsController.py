e60919758e8be96d0e79568fb0bba19cfce71b23
vishnusb1996 <52482641+vishnusb1996@users.noreply.github.com>
2020-09-02 18:38:41 +0530

    Fixed the bug on adding the webapi for products

--- ProductsController.py
+++ ProductsController.py
@@ -13,9 +13,99 @@ def products():
 	try:
 		conn = mysql.connect()
 		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		cursor.execute(dbconstants.SELECT_ALL_CATEGORIES_SQL)
+		cursor.execute(dbconstants.SELECT_ALL_PRODUCTS_SQL)
 		rows = cursor.fetchall()
 		resp = jsonify(rows)
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+@productAPI.route('/createproduct', methods=['POST'])
+def add_product():
+	try:
+		_json = request.json
+		_product_name = _json['productName']
+		_category_id = _json['categoryId']
+		_UnitPrice = _json['unitPrice']
+		# validate the received values
+		if _product_name and _category_id and _UnitPrice and request.method == 'POST':
+		
+			# save edits
+			sql = dbconstants.INSERT_PRODUCT_SQL
+			data = (_product_name, _category_id,_UnitPrice)
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute(sql, data)
+			conn.commit()
+			resp = jsonify('Product added successfully!')
+			resp.status_code = 200
+			return resp
+		else:
+			return not_found()
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+@productAPI.route('/product/<int:id>')
+def product(id):
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute(dbconstants.SELECT_PRODUCT_SQL, id)
+		row = cursor.fetchone()
+		resp = jsonify(row)
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+@productAPI.route('/UpdateProduct', methods=['POST'])
+def update_product():
+	try:
+		_json = request.json
+		_product_id = _json['productid']
+		_product_name = _json['productName']
+		_category_id = _json['categoryId']
+		_UnitPrice = _json['unitPrice']
+		
+		# validate the received values
+		if _product_id and _product_name and _category_id and _UnitPrice and request.method == 'POST':
+			
+			# save edits
+			sql = dbconstants.UPDATE_PRODUCT_SQL
+			data = (_product_name, _category_id,_UnitPrice, _product_id)
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute(sql, data)
+			conn.commit()
+			resp = jsonify('Product updated successfully!')
+			resp.status_code = 200
+			return resp
+		else:
+			return not_found()
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+@productAPI.route('/DeleteCategory/<int:id>')
+def delete_category(id):
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor()
+		cursor.execute(dbconstants.DELETE_PRODUCT_SQL, (id,))
+		conn.commit()
+		resp = jsonify('Product Deleted successfully!')
 		resp.status_code = 200
 		return resp
 	except Exception as e:
--- dbconstants.py
+++ dbconstants.py
@@ -1,3 +1,7 @@
-SELECT_ALL_CATEGORIES_SQL = 'SELECT categoryid, categoryname, description FROM categories';
-# SELECT_ALL_PRODUCTS_SQL = 'SELECT productid, productname,CategoryID,UnitPrice FROM product';
-GetAllCustomers_SQL = 'SELECT * FROM Customers';
+SELECT_ALL_CATEGORIES_SQL = 'SELECT categoryid, categoryname, description FROM categories'
+GetAllCustomers_SQL = 'SELECT * FROM Customers'
+SELECT_ALL_PRODUCTS_SQL = 'SELECT p.ProductID, p.ProductName, p.CategoryID,p.UnitPrice, c.CategoryName, c.Description FROM product p INNER JOIN categories c ON p.CategoryID = c.CategoryID'
+SELECT_PRODUCT_SQL = 'SELECT p.ProductID, p.ProductName, p.CategoryID,p.UnitPrice, c.CategoryName, c.Description FROM product p INNER JOIN categories c ON p.CategoryID = c.CategoryID WHERE p.ProductID=%s'
+DELETE_PRODUCT_SQL = "DELETE FROM product WHERE ProductID=%s"
+INSERT_PRODUCT_SQL = "INSERT INTO product (ProductName,CategoryID,UnitPrice) VALUES (%s,%s,%s)"
+UPDATE_PRODUCT_SQL = "UPDATE product SET ProductName = %s,CategoryID = %s,UnitPrice =%s WHERE ProductID = %s"
